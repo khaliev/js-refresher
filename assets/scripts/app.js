d9157fa0525145200526d1addeb2b0ed1cb5a503
khaliev <dev.adlan@icloud.com>
2025-12-09 21:34:18 +0100

    different ways of exporting & importing2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,7 @@
 // 1. you can import the default export with any name you like, but you do NEED to assign a name
 import apikKey from "./util.js";
 
-console.log(apikKey);
+// console.log(apiKey);
 
 // 2. if you want to import a named export, you MUST use the same name as the exported variable
 // import { apikKey } from "./util.js";
@@ -14,3 +14,17 @@ import * as util from "./util.js";
 console.log(util);
 console.log(util.abc);
 console.log(util.def);
+console.log(util.default);
+
+// 3. you can mix default and named imports in one line:
+import apik, { abc, def } from "./util.js";
+
+console.log(apik);
+console.log(abc);
+console.log(def);
+
+// 4. you can also rename named imports using "as":
+import { abc as myAbc, def as myDef } from "./util.js";
+
+console.log(myAbc);
+console.log(myDef);
