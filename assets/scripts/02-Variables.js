487cc769768817602a255a790a0a5e5da789c97f
khaliev <dev.adlan@icloud.com>
2026-04-27 10:42:34 +0200

    variables

---
+++ assets/scripts/02-Variables.js
@@ -0,0 +1,8 @@
+// --- VARIABLES ---
+
+// variables are like containers that hold data, they allow us to store and manipulate information in our programs
+// in JavaScript, we can declare variables using var, let, or const keywords
+
+var myAbc = "Hello, I'm a variable declared with var!";
+let myDef = "Hi, I'm a variable declared with let!";
+const myGhi = "Hey, I'm a constant variable declared with const!";
