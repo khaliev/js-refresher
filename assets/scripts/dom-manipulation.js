475e26d1d20829d4487eb3dce7eb01d424f43ecc
khaliev <dev.adlan@icloud.com>
2026-05-30 16:34:15 +0200

    dom manipulation

---
+++ assets/scripts/dom-manipulation.js
@@ -0,0 +1,28 @@
+// DOM Manipulation
+
+// Example: Changing the text content of an element
+const heading = document.querySelector("h1");
+heading.TextContent = "Hello!";
+
+// Example: Adding a new element to the DOM
+
+const newParagpraph = document.createElement('p');
+new.paragraph.textContent = "New paragraph";
+document.body.appendChild(newParagraph);
+
+// Example: Removing an element from the DOM
+const oldParagraph = document.querySelector('p');
+oldParagraphe.remove();
+
+// Example: Changing the style of an element
+const box = document.querySelector('.box');
+box.style.backgroundColor = 'blue';
+box.style.width = '200px';
+box.style.height = '200px';
+
+// Example: Adding an event listener to an element
+
+const button = document.addEventListener('click', function() {
+    alert('Button clicked!');
+});
+
